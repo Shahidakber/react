b1c8dddec15e60a9750447ade2829a16bcc4dc00
user <you@example.com>
2021-04-20 12:47:02 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test'
+import TestClass from './Testclass'
 
 function App() {
   return (
     <div className="App">
+       <Test></Test>
+       <Test></Test>
+       <TestClass></TestClass>
       <header className="App-header">
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        hello world
         </p>
         <a
           className="App-link"
